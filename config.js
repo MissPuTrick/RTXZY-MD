64ac0423f745470e77d8c5001a24ebe8ca6d77c4
PuTrick <telmovid.cs@gmail.com>
2023-11-19 20:22:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
+global.owner = ['62895410369812']  
+global.mods = ['62895410369812'] 
+global.prems = ['62895410369812']
 global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.numberowner = '62895410369812' 
 global.mail = 'support@tioprm.my.id' 
 global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
 global.instagram = 'https://instagram.com/prm2.0'
